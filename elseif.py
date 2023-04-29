0a48dbc3d2f43cc52f4f26bf5c44cf8ff2ab8db6
Rajeev Radhakrishnan <scraptorajeev@gmail.com>
2023-04-29 12:13:47 +0530

    elseif

---
+++ elseif.py
@@ -0,0 +1,21 @@
+def agedetails():
+    print("Age")
+    Age=int(input('enter Age: '))
+
+    if Age==18:
+        print("You Are In")
+    elif Age>18 and Age<25:
+        print("You're Eligible")
+    elif Age>26 and Age<30:
+        print("Processing")
+    else:
+        print("Processed")
+
+for Anvarlearncode in range(10):
+    print("Anvar Learn Code")
+
+
+
+
+agedetails()
+
